2347cba1ef6b538ba457444644d80e6ea2083c7e
Nathan Birch <birchn@byui.edu>
2023-01-05 10:00:02 -0700

    adding routes and controllers folders

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/url', (req, res, next) => {
-  res.json(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food']);
-});
+app.use('/', require('./routes'));
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
