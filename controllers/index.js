4eeb90dab1d81952889795d563178f3df99284c9
carly-west <carly.west@byui.edu>
2023-01-18 18:01:19 -0700

    create and update contact

--- controllers/index.js
+++ controllers/index.js
@@ -21,4 +21,42 @@ const getSingleContacts = async (req, res, next) => {
   });
 };
 
-module.exports = { getAllContacts, getSingleContacts };
+const createNewContact = async (req, res, next) => {
+  await mongodb.getDb().db('sample_data').collection('contacts').insertOne({
+    firstName: 'Carly',
+    LastName: 'West',
+    email: 'carly.west99@gmail.com',
+    favoriteColor: 'blue',
+    birthday: '09-20-1999'
+  });
+
+  const result = await mongodb
+    .getDb()
+    .db('sample_data')
+    .collection('contacts')
+    .find({ firstName: 'Carly' });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
+};
+
+const updateContact = async (req, res, next) => {
+  const userId = new client(req.params.id);
+
+  await mongodb
+    .getDb()
+    .db('sample_data')
+    .collection('contacts')
+    .updateOne(
+      { _id: userId },
+      {
+        $set: { email: 'carly.correa8@gmail.com', LastName: 'Correa' },
+        $currentDate: { lastModified: true }
+      }
+    );
+
+  res.status(200).send('Status: OK');
+};
+
+module.exports = { getAllContacts, getSingleContacts, createNewContact, updateContact };
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,8 @@ router.get('/', contactsController.getAllContacts);
 
 router.get('/:id', contactsController.getSingleContacts);
 
+router.post('/new/contact', contactsController.createNewContact);
+
+router.put('/update/:id', contactsController.updateContact);
+
 module.exports = router;
