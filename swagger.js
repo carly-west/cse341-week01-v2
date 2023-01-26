737cd96dbcb7dc239bbafce7ca01a1939a116f57
carly-west <carly.west@byui.edu>
2023-01-26 16:10:39 -0700

    edit swagger

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: 'https://cse341-week01-v2.onrender.com',
+  host: 'cse341-week01-v2.onrender.com',
   schemes: ['http']
 };
 
